066d6518a5d40c7af31a79d28da8ab22e0db899d
Victor Gonzalez <victor.c.gonzalez@inetum.com>
2022-12-26 15:47:57 +0000

    first commit

---
+++ test/src/AppComponent.js
@@ -0,0 +1,29 @@
+/* eslint-disable no-undef */
+/* eslint-disable import/no-anonymous-default-export */
+import React from "react";
+import ReactDOM from "react-dom";
+
+export default class extends React.Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <span className="tag">
+            {Liferay.Language.get("portlet-namespace")}:
+          </span>
+          <span className="value">{this.props.portletNamespace}</span>
+        </div>
+        <div>
+          <span className="tag">{Liferay.Language.get("context-path")}:</span>
+          <span className="value">{this.props.contextPath}</span>
+        </div>
+        <div>
+          <span className="tag">
+            {Liferay.Language.get("portlet-element-id")}:
+          </span>
+          <span className="value">{this.props.portletElementId}</span>
+        </div>
+      </div>
+    );
+  }
+}
